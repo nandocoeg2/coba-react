2244080aadec890ffae252e05ff9ccac8dd07c72
Fernando Julian <nandokun69@gmail.com>
2023-04-19 17:09:54 +0700

    nambahin button

--- day4/src/components/home.js
+++ day4/src/components/home.js
@@ -1,6 +1,8 @@
 import "../styles/style.css"
 import Head from './navigation';
 import Summary from "./summary";
+import Button from 'react-bootstrap/Button';
+
 
 function Home() {
     return (
@@ -8,6 +10,7 @@ function Home() {
             <Head />
             <div className='Homepage'>
                 <h1>Fernando Julian</h1>
+                <Button variant="outline-dark">Ini Button</Button>
             </div>
             <Summary />
         </div>
